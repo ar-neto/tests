fb9080438ae61abbf5721c385598d50c13d2d5cd
A. F. Neto <51216181+hzeri@users.noreply.github.com>
2021-01-31 03:41:23 +0000

    Update index.js

--- website/netlify-plugin-hello-world/index.js
+++ website/netlify-plugin-hello-world/index.js
@@ -1,14 +1,7 @@
 // index.js
 
 module.exports = {
-  onPreBuild({ utils }) {
-    utils.status.show({
-      // Optional. Default to the plugin’s name followed by a generic title.
-      title: 'Main title',
-      // Required.
-      summary: 'Message below the title',
-      // Optional. Empty by default.
-      text: 'Detailed information shown in a collapsible section',
-    })
+  onPreBuild: () => {
+    console.log('Hello world from onPreBuild event!')
   },
 }
