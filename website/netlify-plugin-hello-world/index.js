9c422fbaeb531f3e6739da54b982c68428c9fae6
A. F. Neto <51216181+hzeri@users.noreply.github.com>
2021-01-31 02:28:21 +0000

    Update index.js

--- website/netlify-plugin-hello-world/index.js
+++ website/netlify-plugin-hello-world/index.js
@@ -1,7 +1,7 @@
-  // index.js
+// index.js
 
 module.exports = {
-    onSuccess: () => {
-     console.log('Hello world from onPreBuild event!')
-    },
-  }
+  onPreBuild: () => {
+    console.log('Hello world from onPreBuild event!')
+  },
+}
